6f6cbeac5f766ed987df5e56f085a0495b6234be
Sebastian Westerholm <westerholm.sebastian@gmail.com>
2024-10-18 17:52:31 +0200

    Game logic in place

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 // Asigning variables
+const showPlayerPoints = document.getElementById("playerPoints");
+const showComputerPoints = document.getElementById("computerPoints");
+const showWinner = document.getElementById("showWinner");
+
 let totalGames = 5;
+let gameRound = 0;
+let playerPoints = 0;
+let computerPoints = 0;
 
 // Computer choice
 function compChoice() {
@@ -19,44 +26,71 @@ function compChoice() {
 const playerRock = document.getElementById("rock");
 const playerPaper = document.getElementById("paper");
 const playerScissor = document.getElementById("scissor");
+let playerMove;
 
 function playerChoice() {
   playerRock.addEventListener("click", () => {
-    return "rock";
+    playerMove = "rock";
+    console.log(playerMove, gameRound);
+    gameRound++;
+    playRound();
   });
 
   playerPaper.addEventListener("click", () => {
-    return "paper";
+    playerMove = "paper";
+    console.log(playerMove, gameRound);
+    gameRound++;
+    playRound();
   });
 
   playerScissor.addEventListener("click", () => {
-    return "scissor";
+    playerMove = "scissor";
+    console.log(playerMove, gameRound);
+    gameRound++;
+    playRound();
   });
 }
 
-// Game loop
-const player = playerChoice;
-const computer = setInterval(compChoice, 3000);
+// Play a round and check winner
+function playRound() {
+  const computerMove = compChoice();
+  console.log(`Computer chose: ${computerMove}`);
 
-function checkWinner(p1, pc) {
-  let gameRound = 0;
+  checkWinner(playerMove, computerMove);
 
-  console.log("Entering game loop");
-  while (totalGames > 0 && gameRound > 1) {
-    if (gameRound % 2 === 0) {
-      console.log("Players turn:");
-      p1;
-      console.log("Player plays");
+  totalGames--;
+  if (totalGames === 0) {
+    console.log("Game over!");
+    showWinner.style.display = "block";
+
+    if (playerPoints === computerPoints) {
+      showWinner.innerText = "Tied!";
+    } else if (playerPoints > computerPoints) {
+      showWinner.innerText = "You win!";
     } else {
-      pc;
-      console.log("Computer plays");
+      showWinner.innerText = "Computer wins!";
     }
-
-    gameRound++;
-    totalGames--;
   }
-  console.log(gameRound);
-  console.log("Working fine");
 }
 
-checkWinner(player, computer);
+function checkWinner(player, computer) {
+  if (player === computer) {
+    console.log("It's a tie");
+  } else if (
+    (player === "rock" && computer === "scissor") ||
+    (player === "paper" && computer === "rock") ||
+    (player === "scissor" && computer === "paper")
+  ) {
+    console.log("Player wins!");
+    playerPoints++;
+    showPlayerPoints.innerText = `Points: ${playerPoints}`;
+  } else {
+    console.log("Computer wins!");
+    computerPoints++;
+    showComputerPoints.innerText = `Points: ${computerPoints}`;
+  }
+}
+
+showPlayerPoints.innerText = `Points: ${playerPoints}`;
+showComputerPoints.innerText = `Points: ${computerPoints}`;
+playerChoice();
